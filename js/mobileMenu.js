a5a16d2412d96b1d2f2ea2af879fa1d3e4f5fdba
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2018-08-16 13:39:00 -0300

    Añadidas cabecera y menú responsive (primera versión)

---
+++ js/mobileMenu.js
@@ -0,0 +1,23 @@
+$(function(){
+    let menuVisible = false; 
+    
+    let menuOpen = $("#menuOpen");
+    let menuClose = $("#menuClose"); 
+    
+    menuOpen.on("click", function(){
+        if (!menuVisible) {
+            console.log("foo"); 
+            $("#mobileMenu").animate({left:"0vw"}, "fast", function(){
+                menuVisible = true; 
+            });
+        }
+    });
+    
+    menuClose.on("click", function(){
+        if (menuVisible) {
+            $("#mobileMenu").animate({left:"-50vw"}, "fast", function(){
+                menuVisible = false; 
+            });
+        }
+    });
+});
